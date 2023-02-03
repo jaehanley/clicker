a0bbe152139c62265fe2590e4ae8b7fc69207227
Jae Hanley <hi@jaehanley.com>
2023-02-03 10:07:17 -0500

    Initial Commit

---
+++ clicker/clickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  clickerApp.swift
+//  clicker
+//
+//  Created by Jae Hanley on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct clickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
